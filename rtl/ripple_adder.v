7c3b82f774c0ab4778ecf88964970f30433847df
karimmahmoud22 <karimmohamed003@eng-st.cu.edu.eg>
2022-11-01 22:00:55 +0200

    AdderPlus, RippleAdder and TestBench .v files

--- rtl/ripple_adder.v
+++ rtl/ripple_adder.v
@@ -1,24 +1,24 @@
 `include "full_adder.v"
  
 module ripple_adder
-  #(parameter WIDTH=64)
+  #(parameter N=16)
   (
-   input [WIDTH-1:0] i_add1,
-   input [WIDTH-1:0] i_add2,
-   output [WIDTH-1:0]  o_result,
+   input [N-1:0] i_add1,
+   input [N-1:0] i_add2,
+   output [N-1:0]  o_result,
    output o_carry
    );
 
 
-  wire [WIDTH:0]     w_C;
+  wire [N:0]     w_C;
 
   assign w_C[0]=0;
-  assign o_carry=w_C[WIDTH];
+  assign o_carry=w_C[N];
 
   // Create the Full Adders
   genvar             ii;
   generate
-    for (ii=0; ii<WIDTH; ii=ii+1) 
+    for (ii=0; ii<N; ii=ii+1) 
       begin
         full_adder full_adder_inst
             ( 
